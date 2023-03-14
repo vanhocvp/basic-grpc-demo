5f671debc303d98686ac152776c33f40245567d4
vanhocvp <vanhoc2kvp@gmail.com>
2023-03-14 14:31:17 +0700

    with submodule

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -10,6 +10,6 @@ PROTO_FILES=$(find $PROTO_DIR -name "*.proto")
 for proto_file in $PROTO_FILES; do
     echo $proto_file
     # Sử dụng lệnh protoc để tạo file .go
-    protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative $proto_file
+    protoc --go_out=protos --go-grpc_out=protos $proto_file
     # protoc --go_out="$PROTO_DIR" "$proto_file"
 done
